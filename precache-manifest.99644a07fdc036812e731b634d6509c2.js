f3157fc5c32125e3ca0f7ea3739e73f766a1b0c0
Kinali94 <gandhikinali1994@gmail.com>
2020-08-05 16:16:52 +0530

    Updates

---
+++ precache-manifest.99644a07fdc036812e731b634d6509c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f4f442105f19449922b6e7b0fbde983",
+    "url": "/%7BPortfolio%7D/index.html"
+  },
+  {
+    "revision": "bed5f4f12063fbd11ddd",
+    "url": "/%7BPortfolio%7D/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "bed5f4f12063fbd11ddd",
+    "url": "/%7BPortfolio%7D/static/js/2.f86f764a.chunk.js"
+  },
+  {
+    "revision": "7bfaa32b315fe466d84ab2ae49052d17",
+    "url": "/%7BPortfolio%7D/static/js/2.f86f764a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df1dec165d964792cc0",
+    "url": "/%7BPortfolio%7D/static/js/main.bbc08c6b.chunk.js"
+  },
+  {
+    "revision": "c984011e039e5b76ab45",
+    "url": "/%7BPortfolio%7D/static/js/runtime-main.f707b660.js"
+  }
+]);
